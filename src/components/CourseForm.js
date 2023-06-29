b0ee482f28c3cdaee3f4cb0ce6462c0b839b0365
eminkmru <eminkumru135@outlook.com>
2023-06-29 17:46:31 +0300

    setup the files of the new project

---
+++ src/components/CourseForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CourseForm() {
+  return <div>ssssss</div>;
+}
+
+export default CourseForm;
---
+++ src/components/CourseValue.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CourseValue() {
+  return <div></div>;
+}
+
+export default CourseValue;
